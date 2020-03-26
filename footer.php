0d8d5bdb4ddc8af143a470586890ca85979811eb
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-03-26 23:59:27 +0900

    ヘッダーとフッターのデザイン

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer>
+        <footer>
 			<small>&copy;<?php bloginfo('name') ?>. This WordPress theme developed by <a href="https://github.com/rockhopper-penguin" target="_blank">Rockhopper-Penguin</a>.</small>
 		</footer>
 		<?php wp_footer() ?>
