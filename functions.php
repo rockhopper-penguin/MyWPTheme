f2f87522267d6ad4686105edad8ccc899911145a
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-03-26 23:20:05 +0900

    normalize.cssの読み込み

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 	function getStyle() {
-	wp_enqueue_style( 'style', get_stylesheet_uri());
+		wp_enqueue_style( 'style', get_stylesheet_uri());
+		wp_enqueue_style( 'normalize',get_template_directory_uri() . '/normalize.css');
 	}
 	add_action( 'wp_enqueue_scripts', 'getStyle');
