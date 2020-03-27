bca6e3471a8e95edbc1caddaa89459f8cef84c6d
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-03-27 23:53:51 +0900

    記事のタイトルを出力

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>
+			<?php wp_title('>', true, 'right'); ?>
 			<?php bloginfo('name'); ?>
 		</title>
 		<?php wp_head(); ?>
