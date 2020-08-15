f8bb34bff3d928f9855ddbf9df78493e1577110e
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-08-15 22:35:40 +0900

    animate.cssの読み込み

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 	function getStyle() {
 		wp_enqueue_style( 'style', get_stylesheet_uri());
 		wp_enqueue_style( 'normalize',get_template_directory_uri() . '/normalize.css');
+		wp_enqueue_style( 'animate',get_template_directory_uri() . '/animate.min.css');
 	}
 	add_action( 'wp_enqueue_scripts', 'getStyle');
 	register_sidebar();
